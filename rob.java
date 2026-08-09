1d057089fd96f8e11d034905263190e01448a859
sanikadeshpande515-debug <sanikadeshpande515@gmail.com>
2026-08-09 21:57:21 +0530

    Implement house robber solution in rob.java

---
+++ rob.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev1=0;
+        int prev2=0;
+
+        for(int i=0;i<nums.length;i++){
+            int current =Math.max(nums[i]+prev2,prev1);
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
+}
