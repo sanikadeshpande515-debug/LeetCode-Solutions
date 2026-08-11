d03f133c81dfd7b84c07119eb9827e1cfe15d9ab
sanikadeshpande515-debug <sanikadeshpande515@gmail.com>
2026-08-11 23:39:53 +0530

    Create predictPartyVictory.java

---
+++ predictPartyVictory.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant=new LinkedList<>();
+        Queue<Integer> dire=new LinkedList<>();
+
+        int n=senate.length();
+
+        for(int i=0;i<n;i++){
+            if(senate.charAt(i)=='R'){
+                radiant.add(i);
+            }else{
+                dire.add(i);
+            }
+        }
+        while(!radiant.isEmpty() && !dire.isEmpty()){
+            int r= radiant.poll();
+            int d= dire.poll();
+            if(r<d){
+                radiant.add(r+n);
+            }else{
+                dire.add(d+n);
+            }
+        }
+        if(!radiant.isEmpty()){
+            return"Radiant";
+        }
+        else{
+            return "Dire";
+        }
+        
+    }
+}
