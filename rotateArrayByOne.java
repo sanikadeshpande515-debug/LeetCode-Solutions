022033f5af14fc7a77ad7025ef17cdb6d63a27ae
sanikadeshpande515-debug <sanikadeshpande515@gmail.com>
2026-09-02 22:07:44 +0530

    Create rotateArrayByOne.java

---
+++ rotateArrayByOne.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotateArrayByOne(int[] nums) {
+        // Store the first element in a temporary variable
+        int temp = nums[0];
+        
+        // Shift elements to the left
+        for (int i = 1; i < nums.length; i++) {
+            nums[i - 1] = nums[i];
+        }
+
+        // Place the first element at the end
+        nums[nums.length - 1] = temp;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5};
+
+        solution.rotateArrayByOne(nums);
+
+        // Output the rotated array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
